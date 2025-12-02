9676de6b96607aad58becab1ab8591624c94ea85
normanchan900427 <128122564+normanchan900427@users.noreply.github.com>
2025-12-02 17:19:28 +0800

    Delete __pycache__/build/lib.win-amd64-cpython-38/tensormask directory

--- __pycache__/build/lib.win-amd64-cpython-38/tensormask/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-from .config import add_tensormask_config
-# no fucking idea why this is causing bug (circular import). By Lucas
-# from .arch import TensorMask
