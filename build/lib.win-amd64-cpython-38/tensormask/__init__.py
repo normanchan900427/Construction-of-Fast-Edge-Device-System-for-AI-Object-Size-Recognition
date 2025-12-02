34fbfe9215c3f4404466102a8f207061247dae39
normanchan900427 <128122564+normanchan900427@users.noreply.github.com>
2025-12-02 17:20:24 +0800

    Add files via upload

---
+++ build/lib.win-amd64-cpython-38/tensormask/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+from .config import add_tensormask_config
+# no fucking idea why this is causing bug (circular import). By Lucas
+# from .arch import TensorMask
